f1aaf0ed65c7c887f2a7d56a022c35adb17196b8
Eternal <eternal.proglife@gmail.com>
2024-01-05 12:29:31 +0200

    Januar 5, 2024. Quite a difficult problem.

---
+++ 300-Longest Increasing Subsequence.cpp
@@ -0,0 +1,31 @@
+/*class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, 1);
+        for (int i = 0; i < n; ++i)
+            for (int j = 0; j < i; ++j)
+                if (nums[i] > nums[j] && dp[i] < dp[j] + 1)
+                    dp[i] = dp[j] + 1;
+        return *max_element(dp.begin(), dp.end());
+    }
+};*/
+
+
+class Solution { // 3 ms, faster than 98.69%
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> sub;
+        for (int x : nums) {
+            if (sub.empty() || sub[sub.size() - 1] < x) {
+                sub.push_back(x);
+            } else {
+                auto it = lower_bound(sub.begin(), sub.end(), x); // Find the index of the first element >= x
+                *it = x; // Replace that number with x
+            }
+        }
+        return sub.size();
+    }
+};
+
+// Explanation: https://leetcode.com/problems/longest-increasing-subsequence/solutions/1326308/c-python-dp-binary-search-bit-segment-tree-solutions-picture-explain-o-nlogn/?envType=daily-question&envId=2024-01-05
