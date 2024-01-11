59d03e1eac5afb90a2c85dc5352ab5657fd8f99e
Eternal <eternal.proglife@gmail.com>
2024-01-11 18:17:24 +0200

    Januar 11, 2024. Medium BT Problem.

---
+++ 1026-Maximum Difference Between Node and Ancestor.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    int result = 0;
+
+    int maxAncestorDiff(TreeNode* root) {
+        if (root == nullptr) return 0;
+
+        dfs(root, root->val, root->val);
+        return result;
+    }
+
+    void dfs(TreeNode* root, int curMax, int curMin) {
+        if (root == nullptr) return;
+
+        int possibleResult = max(abs(curMax - root->val), abs(curMin - root->val));
+        result = max(result, possibleResult);
+
+        curMax = max(root->val, curMax);
+        curMin = min(root->val, curMin);
+
+        dfs(root->left, curMax, curMin);
+        dfs(root->right, curMax, curMin);
+
+        return;
+    }
+ 
+};
