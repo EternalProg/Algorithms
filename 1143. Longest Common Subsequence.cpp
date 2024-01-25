ed822b8294c832effa6c93247ec264eaee55ccdc
Eternal <eternal.proglife@gmail.com>
2024-01-25 12:33:17 +0200

    Januar 25, 2024. Medium problem: String, Dynamic Programming.

--- 1143. Longest Common Subsequence.cpp
+++ 1143. Longest Common Subsequence.cpp
@@ -1,18 +1,41 @@
+// Tabulation
 class Solution {
-public:
-    int longestCommonSubsequence(string &text1, string &text2) {
-        int n = text1.size(), m = text2.size();
-        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
-        for(int j = 0;j<=m;++j) dp[0][j] = 0;
-        for(int i = 0;i<=n;++i) dp[i][0] = 0;
-        for(int i = 1;i<=n; ++i) {
-            for(int j = 1; j<=m; ++j) {
-                if(text1[i-1] == text2[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
-                else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
-            }
-        }
-        return dp[n][m];
+ public:
+  int longestCommonSubsequence(string &text1, string &text2) {
+    int n = text1.size(), m = text2.size();
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+    for (int j = 0; j <= m; ++j) dp[0][j] = 0;
+    for (int i = 0; i <= n; ++i) dp[i][0] = 0;
+    for (int i = 1; i <= n; ++i) {
+      for (int j = 1; j <= m; ++j) {
+        if (text1[i - 1] == text2[j - 1])
+          dp[i][j] = 1 + dp[i - 1][j - 1];
+        else
+          dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+      }
     }
+    return dp[n][m];
+  }
+};
+// Space Optimised
+class Solution {
+ public:
+  int longestCommonSubsequence(string &text1, string &text2) {
+    int n = text1.size(), m = text2.size();
+    vector<int> prev(m + 1, 0), cur(m + 1, 0);
+    for (int j = 0; j <= m; ++j) prev[j] = 0;
+
+    for (int i = 1; i <= n; ++i) {
+      for (int j = 1; j <= m; ++j) {
+        if (text1[i - 1] == text2[j - 1])
+          cur[j] = 1 + prev[j - 1];
+        else
+          cur[j] = max(prev[j], cur[j - 1]);
+      }
+      prev = cur;
+    }
+    return prev[m];
+  }
 };
 
 // https://youtu.be/NPZn9jBrX8U
