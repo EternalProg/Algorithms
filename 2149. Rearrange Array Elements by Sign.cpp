564ee911535c9ae56f286df2a04e143bb68a0117
EternalProg <eternal.proglife@gmail.com>
2024-02-14 17:31:06 +0200

    February 14, 2024. Medium problem: Array, Two Pointers.

---
+++ 2149. Rearrange Array Elements by Sign.cpp
@@ -0,0 +1,17 @@
+class Solution {
+ public:
+  vector<int> rearrangeArray(vector<int>& nums) {
+    int pos = 0, neg = 1;
+    vector<int> ans(nums.size());
+    for (int i = 0; i < nums.size(); ++i) {
+      if (nums[i] > 0) {
+        ans[pos] = nums[i];
+        pos += 2;
+      } else {
+        ans[neg] = nums[i];
+        neg += 2;
+      }
+    }
+    return ans;
+  }
+};
