5422a0952305b9bae56c5d1d4e16bc73b51d7e87
EternalProg <eternal.proglife@gmail.com>
2024-02-16 20:07:25 +0200

    February 16, 2024. Medium problem: Array, Hash Table, Greedy, Sorting, Counting.

---
+++ 1481. Least Number of Unique Integers after K Removals.cpp
@@ -0,0 +1,30 @@
+const int ZERO = []() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  return 0;
+}();
+
+class Solution {
+ public:
+  int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
+    unordered_map<int, int> mp;
+    for (auto numb : arr) {
+      ++mp[numb];
+    }
+
+    vector<pair<int, int>> sortedPairs(mp.begin(), mp.end());
+    sort(sortedPairs.begin(), sortedPairs.end(),
+         [](const auto& a, const auto& b) { return a.second < b.second; });
+
+    int ans = mp.size();
+    for (const auto& pair : sortedPairs) {
+      k -= pair.second;
+      if (k >= 0) {
+        --ans;
+      } else
+        break;
+    }
+
+    return ans;
+  }
+};
