a5032fb9f2cd0a83caa1c50ddf3b6b764000f273
EternalProg <eternal.proglife@gmail.com>
2024-02-26 20:35:16 +0200

    February 26, 2024. easy Problem: Tree, Depth-First Search, Breadth-First Search, Binary Tree.

--- BinaryTree/100. Same Tree.cpp
+++ BinaryTree/100. Same Tree.cpp
@@ -27,3 +27,32 @@ public:
         return trav(p, q);
     }
 };
+
+ /**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ 
+const int ZERO = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return 0;
+}();
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (!p || !q)
+            return p == q;
+        if (p->val != q->val)
+            return false;
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
+*/
