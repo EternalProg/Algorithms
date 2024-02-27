5b2711075e9025c29e17b8dd50c3aed6e8e4711d
EternalProg <eternal.proglife@gmail.com>
2024-02-27 11:59:30 +0200

    February 27, 2024. Tree, Depth-First Search, Binary Tree.

--- BinaryTree/543. Diameter of Binary Tree.cpp
+++ BinaryTree/543. Diameter of Binary Tree.cpp
@@ -31,5 +31,38 @@ public:
         return diameter;
     }
 };
+// const int ZERO = []() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(nullptr);
+//     return 0;
+// }();
+// class Solution {
+// public:
+//     pair<int,int> solve(TreeNode *root){
+//         if(root==NULL){
+//             pair<int,int>temp = make_pair(0,0);
+//             return temp;
+//         }
+//
+//         pair<int,int>left= solve(root->left);
+//         pair<int,int>right =solve(root->right);
+//
+//         int op1=left.first;
+//         int op2=right.first;
+//         int op3 = left.second +right.second;
+//
+//         pair<int,int>ans;
+//         ans.first= max(op1 , max(op2, op3));
+//         ans.second = max(left.second ,right.second) +1;
+//
+//         return ans;
+//
+//
+//     }
+//     int diameterOfBinaryTree(TreeNode* root) {
+//         return solve(root).first;
+//         
+//     }
+// };
 
 // https://takeuforward.org/data-structure/calculate-the-diameter-of-a-binary-tree/
