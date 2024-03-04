d649a908e7f25b4707d0a8a41dfedaa61e404846
EternalProg <eternal.proglife@gmail.com>
2024-03-04 16:55:57 +0200

    Marz 4, 2024. Medium problem: Array, Two Pointers, Greedy, Sorting.

---
+++ 948. Bag of Tokens.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int bagOfTokensScore(vector<int>& tokens, int power) {
+        int low = 0;
+        int high = tokens.size() - 1;
+        int score = 0;
+        sort(tokens.begin(), tokens.end());
+
+        while (low <= high) {
+            // When we have enough power, play lowest token face-up
+            if (power >= tokens[low]) {
+                score += 1;
+                power -= tokens[low];
+                low += 1;
+            // We don't have enough power to play a token face-up
+            // If there is at least one token remaining,
+            // and we have enough score, play highest token face-down
+            } else if (low < high && score > 0) {
+                score -= 1;
+                power += tokens[high];
+                high -= 1;
+            // We don't have enough score, power, or tokens 
+            // to play face-up or down and increase our score
+            } else {
+                return score;
+            }
+        }
+        return score;
+    }
+};
