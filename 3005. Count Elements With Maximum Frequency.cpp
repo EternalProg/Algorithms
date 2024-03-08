1bfef6aecdd72110e9c9b18b795a440f7aab4cb4
EternalProg <eternal.proglife@gmail.com>
2024-03-08 10:50:41 +0200

    Marz 8, 2024. Easy prolem: Array, Hash Table, Counting.

---
+++ 3005. Count Elements With Maximum Frequency.cpp
@@ -0,0 +1,51 @@
+/*class Solution {
+public:
+    int maxFrequencyElements(vector<int>& nums) {
+        unordered_map<int, int> mp;
+
+        for(auto numb: nums) {
+            ++mp[numb];
+        }
+
+        int maxLength = 0;
+        for(auto n: mp) {
+            maxLength = max(maxLength, n.second);
+        }
+
+        int ans = 0;
+        for(auto pair: mp) {
+            if(pair.second == maxLength) ++ans;
+        }
+
+        return ans * maxLength;
+    }
+};*/
+
+class Solution {
+public:
+    int maxFrequencyElements(vector<int>& nums) {
+        unordered_map<int, int> frequencies;
+        int maxFrequency = 0;
+        int totalFrequencies = 0;
+
+        // Find the frequency of each element
+        // Determine the maximum frequency
+        // Calculate the total frequencies of elements with the maximum frequency
+        for (int num : nums) {
+            frequencies[num]++;
+            int frequency = frequencies[num];
+
+            // If we discover a higher frequency element
+            // Update maxFrequency
+            // Re-set totalFrequencies to the new max frequency
+            if (frequency > maxFrequency) {
+                maxFrequency = frequency;
+                totalFrequencies = frequency;
+                // If we find an element with the max frequency, add it to the total
+            } else if (frequency == maxFrequency) {
+                totalFrequencies += frequency;
+            }
+        }
+        return totalFrequencies;
+    }
+};
