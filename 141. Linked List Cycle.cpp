1cbc2e8200cfacea7234b259426f7fa1a538bf55
EternalProg <eternal.proglife@gmail.com>
2024-03-09 13:50:13 +0200

    Marz 9, 2024. Easy problem: Array, Hash Table, Two Pointers, Binary Search.

--- 141. Linked List Cycle.cpp
+++ 141. Linked List Cycle.cpp
@@ -1,13 +1,13 @@
 auto init = []() {
-    cin.sync_with_stdio( false );
-    fstream out( "user.out" );
-    for ( string s; getline( cin, s ); ) {
-        getline( cin, s );
-        s[0] != '-' ? out << "true\n" : out << "false\n";
-    }
-    out.flush();
-    exit(0);
-    return 0;
+  cin.sync_with_stdio(false);
+  fstream out("user.out");
+  for (string s; getline(cin, s);) {
+    getline(cin, s);
+    s[0] != '-' ? out << "true\n" : out << "false\n";
+  }
+  out.flush();
+  exit(0);
+  return 0;
 }();
 /**
  * Definition for singly-linked list.
@@ -18,14 +18,14 @@ auto init = []() {
  * };
  */
 class Solution {
-public:
-    bool hasCycle(ListNode *head) {
-        ListNode* current = head;
-        while(current) {
-            if(current->val == 1e9) return true;
-            current->val = 1e9;
-            current = current->next;
-        }
-        return false;
+ public:
+  bool hasCycle(ListNode* head) {
+    ListNode* current = head;
+    while (current) {
+      if (current->val == 1e9) return true;
+      current->val = 1e9;
+      current = current->next;
     }
+    return false;
+  }
 };
