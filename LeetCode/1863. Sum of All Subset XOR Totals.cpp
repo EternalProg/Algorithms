3d3aff1911c764ff42d0e17d90b3ae17e60e9b32
EternalProg <eternal.proglife@gmail.com>
2024-05-20 11:17:54 +0300

    May 20, 2024. Easy problem: Array, Math, Backtracking, Bit Manipulation, Combinatorics, Enumeration.

---
+++ LeetCode/1863. Sum of All Subset XOR Totals.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int subsetXORSum(vector<int>& nums) {
+        return dfs(nums, 0, 0);
+    }
+    
+private:
+    int dfs(vector<int>& nums, int index, int currentXor) {
+        // Base case: when all elements have been considered
+        if (index == nums.size()) {
+            return currentXor;
+        }
+        // Include nums[index] in the subset
+        int include = dfs(nums, index + 1, currentXor ^ nums[index]);
+        // Exclude nums[index] from the subset
+        int exclude = dfs(nums, index + 1, currentXor);
+        return include + exclude;
+    }
+};
