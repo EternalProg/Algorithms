aadb9e43d05f28f7fb909d51ddb43853d7536269
EternalProg <eternal.proglife@gmail.com>
2024-06-07 14:08:57 +0300

    June 7, 2024. Medium problem: Array, Hash Table, String, Trie.

---
+++ LeetCode/648. Replace Words.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string replaceWords(vector<string>& dictionary, string sentence) {
+        set<string> st;
+        string word = "", ans = "";
+        sentence += ' ';
+
+        for (auto i : dictionary)
+            st.insert(i);
+
+        for (auto i : sentence) {
+            if (i == ' ') {
+                if (ans.size() > 0)
+                    ans += ' ';
+
+                string temp = "";
+                for (auto j : word) {
+                    temp += j;
+                    if (st.count(temp)) {
+                        break;
+                    }
+                }
+                ans += temp;
+                word = "";
+            } else
+                word += i;
+        }
+
+        return ans;
+    }
+};
