ba95c4e38fdd0a2da9190b8121fbade852582f4f
EternalProg <eternal.proglife@gmail.com>
2024-06-13 10:15:54 +0300

    June 13, 2024. Easy problem: Array,  Greedy, Sorting.

---
+++ LeetCode/2037. Minimum Number of Moves to Seat Everyone.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+        int ans = 0;
+        sort(seats.begin(), seats.end());
+        sort(students.begin(), students.end());
+        for(int i = 0; i < students.size(); ++i) {
+            ans += abs(seats[i] - students[i]);
+        }
+        return ans;
+    }
+};
