0fe9c1d29f825d0e232a241afe7ff44a2e049ea3
EternalProg <eternal.proglife@gmail.com>
2024-06-26 09:52:12 +0300

    June 26, 2024. Medium problem: Divide and Conquer, Greedy, Tree, Depth-First Search, Binary Search Tree, Binary Tree.

--- LeetCode/gcommit.sh
+++ LeetCode/gcommit.sh
@@ -1,12 +1,24 @@
 #!/bin/sh
-LAST_ADDED_FILE=`ls -t | head -1`
 
-echo $LAST_ADDED_FILE
-COMMIT_MESSAGE=$1
-echo $COMMIT_MESSAGE
+if [ -z "$1" ]; then
+  echo "Коміт-меседж не заданий. Використання: ./gcommit.sh \"commit message\""
+  exit 1
+fi
 
-git add $LAST_ADDED_FILE
-git commit -m $COMMIT_MESSAGE
+LAST_ADDED_FILE=$(ls -t | head -1)
+
+if [ -z "$LAST_ADDED_FILE" ]; then
+  echo "Файл для додавання не знайдено."
+  exit 1
+fi
+
+echo "Останній доданий файл: $LAST_ADDED_FILE"
+
+COMMIT_MESSAGE="$1"
+echo "Commit message: $COMMIT_MESSAGE\n"
+
+git add "$LAST_ADDED_FILE"
+git commit -m "$COMMIT_MESSAGE"
 git push
 
-echo "File $LAST_ADDED_FILE was pushed with commit message $COMMIT_MESSAGE"
+echo "Файл $LAST_ADDED_FILE був доданий і запушений з коміт-меседжем: $COMMIT_MESSAGE"
