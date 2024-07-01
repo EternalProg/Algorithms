df3ab43cd75f33dbcc48fa6ed142c02ae13b34fd
EternalProg <eternal.proglife@gmail.com>
2024-07-01 09:27:32 +0300

    July 1, 2024. Easy problem: Array.

---
+++ LeetCode/1550. Three Consecutive Odds.cpp
@@ -0,0 +1,18 @@
+const int speed = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        if(arr.size() < 3) return false;
+        for(int i = 0; i <= arr.size()-3; ++i) {
+            if(arr[i] % 2 != 0 && arr[i+1] % 2 != 0 && arr[i+2] % 2 != 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
