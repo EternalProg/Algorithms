53023130c6b555ab57ab47c1b978f3ca5b85ec41
EternalProg <eternal.proglife@gmail.com>
2024-07-02 14:19:35 +0300

    Jule 2, 2024. Easy problem: Array, Hash Table, Two Pointers, Binary Search, Sorting.

---
+++ LeetCode/350. Intersection of Two Arrays II.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> res;
+        unordered_map<int, int> mp;
+        for (int numb : nums1)
+            ++mp[numb];
+        for (int i = 0; i < nums2.size(); ++i) {
+            if (mp[nums2[i]] > 0) {
+                res.push_back(nums2[i]);
+                mp[nums2[i]]--;
+            }
+        }
+        return res;
+    }
+};
